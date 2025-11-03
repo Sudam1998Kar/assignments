6172a8967aa659463738015ae4853b1bf11c090e
Sudam <you@example.com>
2025-11-03 14:43:20 +0530

    Assignment1

---
+++ src/assignments/Assignment1.java
@@ -0,0 +1,103 @@
+package assignments;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//1>Temperature of a city in degrees Celsius: 25.5
+		float temperature= 25.5f;
+		System.out.println("Tempearture of city in degree Celcius is: "+temperature);
+		
+		   //2>Whether a customer has placed an order: true or false
+		    boolean order= true;
+		    System.out.println("Customer has placed an order..."+ order);
+			boolean order1= false;
+			System.out.println("Customer has placed order..."+ order1);
+		
+	
+	       //3>Person's phone number: "123-456-7890"
+		    String phoneNumber= "123-456-7890";
+		    System.out.println("Your phone number is="+ phoneNumber);
+		    
+		    
+		    //4>Amount of money in a customer's bank account: 1000.50
+		    float money= 1000.50f;
+		    System.out.println("Amount of money in a customer's bank account: "+money);
+		    
+		    
+		    //5>Person's email address: "john.doe@example.com"
+		    String email= "john.doe@example.com";
+		    System.out.println("Person's email address: "+email);
+		    
+		    //6>Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+		    float latitude= 37.7749f;
+		    System.out.println("Latitude of earth is: "+latitude);
+		    float logitude= -122.4194f;
+		    System.out.println("longitude of earth is: "+logitude);
+		    
+		    //7>Person's marital status: true or false
+		    
+		    boolean status= true;
+		    System.out.println("is a person married: "+status);
+		    boolean status1= false;
+		    System.out.println("is a person married: "+status1);
+		    
+		    //8>Person's occupation: "Software Engineer"
+		    String occupation= "Software Engineer";
+		    System.out.println("person's occupation: "+occupation);
+		    
+		    //9>Person's favourite colour: "Blue"
+		    String Colour="Blue";
+		    System.out.println("Person's favourite color: "+Colour);
+		    
+		    //10>Current year: 2023
+		    int year= 2023;
+		    System.out.println("Current year is: "+year);
+		    
+		    //11>Number of followers on a social media platform: 1,000,000
+		    int followers= 1000000;
+		    System.out.println("Number of followers on a social media platform: "+followers);
+		    
+		    //12>Rating of a movie: 7.5
+		    double movieRating=7.5;
+		    System.out.println("Rating of a movie: "+movieRating);
+		    
+		    //13>Person's blood type: 'A'
+		    char bloodType= 'A';
+		    System.out.println("Person's blood type: "+bloodType);
+		    
+		    //14>Title of a book: "To Kill a Mockingbird"
+		    String bookTitle="To Kill a Mockingbird";
+		    System.out.println("Title of book: "+bookTitle);
+		    
+		    //15>Number of employees in a company: 500
+		    int employee= 500;
+		    System.out.println("Number of employees in a company: "+employee);
+		    
+		    //16>Time of an event: 2:30 PM
+		    String time= "2:30 PM";
+		    System.out.println("Time of an event "+time);
+		    
+		    //17>Name of a country: "United States"
+		    String country= "United States";
+		    System.out.println("Name of a country: "+country);
+		    
+		    //18>Person's eye color: "Brown"
+		    String eyeColor= "Brown";
+		    System.out.println("Person's eye color: "+eyeColor);
+		    
+		    //19>Person's birthplace: "New York City"
+		    String birthPlace="New York City";
+		    System.out.println("Person's birthplace: "+birthPlace);
+		    
+		    //Distance between two cities: 200.5
+		    float distance= 200.5f;
+		    System.out.println("Distance between two cities: "+distance);
+		    
+		    
+		    
+		   
+		    
+	}
+}
---
+++ src/language/basics/PrintStatement.java
@@ -0,0 +1,19 @@
+package language.basics;
+
+public class PrintStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hi Sudam"); //print on same line
+		System.out.println();
+		System.out.println("Hi Sudam");// print on new line
+		System.out.printf("Hello %s You have new mesaages %d Which are %b","Sudam",5,true);
+		System.out.println();
+		System.err.println("You have got error!");
+
+
+		
+
+	}
+
+}
