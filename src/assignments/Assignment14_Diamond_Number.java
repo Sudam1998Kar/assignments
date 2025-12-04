ff0a1f2f2cec2d2e4e6f519a910151b1831fdffd
Sudam <you@example.com>
2025-12-04 19:15:38 +0530

    Assignment14

---
+++ src/assignments/Assignment14_Diamond_Number.java
@@ -0,0 +1,38 @@
+package assignments;
+
+public class Assignment14_Diamond_Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 int n = 10; // Number of rows in the top half of the diamond
+
+	        // Upper half of the diamond
+	        for (int i = 1; i <= n; i++) {
+	            // Print spaces
+	            for (int j = n; j > i; j--) {
+	                System.out.print(" ");
+	            }
+	            // Print numbers
+	            for (int j = 1; j <= i; j++) {
+	                System.out.print(j + " ");
+	            }
+	            System.out.println();
+	        }
+
+	        // Lower half of the diamond
+	        for (int i = n - 1; i >= 1; i--) {
+	            // Print spaces
+	            for (int j = n; j > i; j--) {
+	                System.out.print(" ");
+	            }
+	            // Print numbers
+	            for (int j = 1; j <= i; j++) {
+	                System.out.print(j + " ");
+	            }
+	            System.out.println();
+
+	}
+
+	}
+}
